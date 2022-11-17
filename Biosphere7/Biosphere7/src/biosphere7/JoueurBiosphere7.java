af51ac8a2f81c88ca16acb50219a87653e4a2d42
qartigala <qartigala@etu.iut-bordeaux.fr>
2022-11-17 13:52:28 +0100

    premier bug régler

---
+++ Biosphere7/Biosphere7/src/biosphere7/JoueurBiosphere7.java
@@ -0,0 +1,69 @@
+package biosphere7;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * Joueur implémentant les actions possibles à partir d'un plateau, pour un
+ * niveau donné.
+ */
+public class JoueurBiosphere7 implements IJoueurBiosphere7 {
+
+    /**
+     * Cette méthode renvoie, pour un plateau donné et un joueur donné, toutes
+     * les actions possibles pour ce joueur.
+     *
+     * @param plateau le plateau considéré
+     * @param couleurJoueur couleur du joueur
+     * @param niveau le niveau de la partie à jouer
+     * @return l'ensemble des actions possibles
+     */
+    @Override
+    public String[] actionsPossibles(Case[][] plateau, char couleurJoueur, int niveau) {
+        // afficher l'heure de lancement
+        SimpleDateFormat format = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss.SSS");
+        System.out.println("actionsPossibles : lancement le " + format.format(new Date()));
+        // se préparer à stocker les actions possibles
+        ActionsPossibles actions = new ActionsPossibles();
+        // calculer les vitalités sur le plateau initial
+        Vitalites vitalites = vitalitesPlateau(plateau);
+        // ajout des actions "planter pommier"
+        for (int lig = 0; lig < Coordonnees.NB_LIGNES; lig++) {
+            for (int col = 0; col < Coordonnees.NB_COLONNES; col++) {
+                Coordonnees coord = new Coordonnees(lig, col);
+                ajoutActionPommier(coord, actions, vitalites, couleurJoueur);
+            }
+        }
+        System.out.println("actionsPossibles : fin");
+        
+        return actions.nettoyer();
+    }
+
+    /**
+     * Somme des vitalités des plantes de chaque joueur sur le plateau.
+     *
+     * @param plateau le plateau
+     * @return la somme des vitalités des plantes de chaque joueur
+     */
+    static Vitalites vitalitesPlateau(Case[][] plateau) {
+        // TODO il y en aura besoin à un moment !
+        return new Vitalites(0, 0);
+    }
+
+    /**
+     * Ajout d'une action de plantation de pommier dans l'ensemble des actions
+     * possibles.
+     *
+     * @param coord coordonnées de la case où planter le pommier
+     * @param actions l'ensemble des actions possibles (en construction)
+     * @param vitalites la somme des vitalités sur le plateau avant de jouer l'action
+     * @param couleur la couleur du pommier à ajouter
+     */
+    void ajoutActionPommier(Coordonnees coord, ActionsPossibles actions,
+            Vitalites vitalites, char couleur) {
+        String action = "P" + coord.carLigne() + coord.carColonne() + "," 
+                + (vitalites.vitalitesRouge + 1) + ","
+                + (vitalites.vitalitesBleu);
+        actions.ajouterAction(action);
+    }
+}
---
+++ Biosphere7/Biosphere7/test/biosphere7/JoueurBiosphere7Test.java
@@ -0,0 +1,204 @@
+package biosphere7;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+
+/**
+ * Tests unitaires de la classe JoueurBiosphere7.
+ */
+public class JoueurBiosphere7Test {
+
+    /**
+     * Test de la fonction actionsPossibles. Commentez les appels aux tests des
+     * niveaux inférieurs, n'activez que le test du niveau à valider.
+     */
+    @Test
+    public void testActionsPossibles() {
+        testActionsPossibles_niveau1();
+        // testActionsPossibles_niveau2();
+    }
+
+    /**
+     * Test de la fonction actionsPossibles, au niveau 1.
+     */
+    public void testActionsPossibles_niveau1() {
+        JoueurBiosphere7 joueur = new JoueurBiosphere7();
+        // un plateau sur lequel on veut tester actionsPossibles()
+        Case[][] plateau = Utils.plateauDepuisTexte(PLATEAU_VIDE);
+        // on choisit la couleur du joueur
+        char couleur = 'R';
+        // on choisit le niveau
+        int niveau = 1;
+        // on lance actionsPossibles
+        String[] actionsPossiblesDepuisPlateau
+                = joueur.actionsPossibles(plateau, couleur, niveau);
+        ActionsPossibles actionsPossibles
+                = new ActionsPossibles(actionsPossiblesDepuisPlateau);
+        // on peut afficher toutes les actions possibles calculées :
+        actionsPossibles.afficher();
+        // on peut aussi tester si une action est dans les actions possibles :
+        assertTrue(actionsPossibles.contient("PaB,1,0"));
+        // on peut aussi tester si une action n'est pas dans les actions 
+        // possibles :
+        assertFalse(actionsPossibles.contient("PaO,1,0"));
+        assertFalse(actionsPossibles.contient("PaA,0,0"));
+        // testons les 4 coins :
+        assertTrue(actionsPossibles.contient("PaA,1,0"));
+        assertTrue(actionsPossibles.contient("PnA,1,0"));
+        assertTrue(actionsPossibles.contient("PaN,1,0"));
+        assertTrue(actionsPossibles.contient("PnN,1,0"));        
+        // vérifions s'il y a le bon nombre d'actions possibles :
+        assertEquals(Coordonnees.NB_LIGNES * Coordonnees.NB_COLONNES,
+                actionsPossiblesDepuisPlateau.length);
+    }
+    
+    /**
+     * Test de la fonction actionsPossibles, au niveau 2.
+     */
+    public void testActionsPossibles_niveau2() {
+        JoueurBiosphere7 joueur = new JoueurBiosphere7();
+        // plateau, couleur et niveau
+        Case[][] plateau = Utils.plateauDepuisTexte(PLATEAU_NIVEAU2);
+        char couleur = 'B';
+        int niveau = 2;
+        // on lance actionsPossibles
+        String[] actionsPossiblesDepuisPlateau
+                = joueur.actionsPossibles(plateau, couleur, niveau);
+        ActionsPossibles actionsPossibles
+                = new ActionsPossibles(actionsPossiblesDepuisPlateau);
+        // testons les 4 coins :
+        assertTrue(actionsPossibles.contient("PaA,2,3"));
+        assertTrue(actionsPossibles.contient("PnA,2,3"));
+        assertFalse(actionsPossibles.contient("PaN,2,3"));
+        assertTrue(actionsPossibles.contient("PnN,2,3"));        
+        // on peut poser sur une case quelconque vide :
+        assertTrue(actionsPossibles.contient("PkD,2,3"));
+        // on ne peut pas poser sur une case occupée :
+        assertFalse(actionsPossibles.contient("PfA,2,3"));
+        assertFalse(actionsPossibles.contient("PeI,2,3"));
+        assertFalse(actionsPossibles.contient("PhJ,2,3"));
+        // nombre correct d'actions possibles :
+        assertEquals(Coordonnees.NB_LIGNES * Coordonnees.NB_COLONNES - 4,
+                actionsPossiblesDepuisPlateau.length);
+    }
+    
+    /**
+     * Test de la fonction ajoutActionPommier.
+     */
+    @Test
+    public void testAjoutActionPommier() {
+        JoueurBiosphere7 joueur = new JoueurBiosphere7();
+        ActionsPossibles actions = new ActionsPossibles();
+        Vitalites vitalites = new Vitalites(0, 0);
+        // pour l'instant pas d'action possible
+        assertEquals(0, actions.nbActions);
+        // on crée le tableau d'actions et on en ajoute une
+        joueur.ajoutActionPommier(Coordonnees.depuisCars('f', 'D'), actions, 
+                vitalites, Case.CAR_ROUGE);
+        // l'action est devenue possible
+        assertTrue(actions.contient("PfD,1,0"));
+        // une action possible mais qui n'a pas encore été ajoutée
+        assertFalse(actions.contient("PbH,1,0"));
+        // pour l'instant une seule action possible
+        assertEquals(1, actions.nbActions);
+        // ajout d'une deuxième action possible
+        joueur.ajoutActionPommier(Coordonnees.depuisCars('b', 'H'), actions, 
+                vitalites, Case.CAR_ROUGE);
+        // l'action a bien été ajoutée
+        assertTrue(actions.contient("PbH,1,0"));
+        // désormais, deux actions possibles
+        assertEquals(2, actions.nbActions);
+    }
+
+    @Test
+    public void testVitalitesPlateau() {
+        // à décommenter le moment venu...
+        /*
+        // plateau : rouge 0, bleu 0
+        Case[][] plateau1 = Utils.plateauDepuisTexte(PLATEAU_VIDE);
+        Vitalites vita1 = JoueurBiosphere7.vitalitesPlateau(plateau1);
+        assertEquals(0, vita1.vitalitesRouge);
+        assertEquals(0, vita1.vitalitesBleu);
+        // plateau : rouge 2, bleu 2
+        Case[][] plateau2 = Utils.plateauDepuisTexte(PLATEAU_NIVEAU2);
+        Vitalites vita2 = JoueurBiosphere7.vitalitesPlateau(plateau2);
+        assertEquals(2, vita2.vitalitesRouge);
+        assertEquals(2, vita2.vitalitesBleu);
+        */
+    }
+    
+    /**
+     * Un plateau de base, sous forme de chaîne. Pour construire une telle
+     * chaîne depuis votre sortie.log, déclarez simplement : final String
+     * MON_PLATEAU = ""; puis copiez le plateau depuis votre sortie.log, et
+     * collez-le entre les guillemets. Puis Alt+Shift+f pour mettre en forme.
+     */
+    final String PLATEAU_VIDE
+            = "   A   B   C   D   E   F   G   H   I   J   K   L   M   N \n"
+            + " +---+---+---+---+---+---+---+---+---+---+---+---+---+---+\n"
+            + "a|   |   |   |   |   |   |   |   |   |   |   |   |   |   |\n"
+            + " +---+---+---+---+---+---+---+---+---+---+---+---+---+---+\n"
+            + "b|   |   |   |   |   |   |   |   |   |   |   |   |   |   |\n"
+            + " +---+---+---+---+---+---+---+---+---+---+---+---+---+---+\n"
+            + "c|   |   |   |   |   |   |   |   |   |   |   |   |   |   |\n"
+            + " +---+---+---+---+---+---+---+---+---+---+---+---+---+---+\n"
+            + "d|   |   |   |   |   |   |   |   |   |   |   |   |   |   |\n"
+            + " +---+---+---+---+---+---+---+---+---+---+---+---+---+---+\n"
+            + "e|   |   |   |   |   |   |   |   |   |   |   |   |   |   |\n"
+            + " +---+---+---+---+---+---+---+---+---+---+---+---+---+---+\n"
+            + "f|   |   |   |   |   |   |   |   |   |   |   |   |   |   |\n"
+            + " +---+---+---+---+---+---+---+---+---+---+---+---+---+---+\n"
+            + "g|   |   |   |   |   |   |   |   |   |   |   |   |   |   |\n"
+            + " +---+---+---+---+---+---+---+---+---+---+---+---+---+---+\n"
+            + "h|   |   |   |   |   |   |   |   |   |   |   |   |   |   |\n"
+            + " +---+---+---+---+---+---+---+---+---+---+---+---+---+---+\n"
+            + "i|   |   |   |   |   |   |   |   |   |   |   |   |   |   |\n"
+            + " +---+---+---+---+---+---+---+---+---+---+---+---+---+---+\n"
+            + "j|   |   |   |   |   |   |   |   |   |   |   |   |   |   |\n"
+            + " +---+---+---+---+---+---+---+---+---+---+---+---+---+---+\n"
+            + "k|   |   |   |   |   |   |   |   |   |   |   |   |   |   |\n"
+            + " +---+---+---+---+---+---+---+---+---+---+---+---+---+---+\n"
+            + "l|   |   |   |   |   |   |   |   |   |   |   |   |   |   |\n"
+            + " +---+---+---+---+---+---+---+---+---+---+---+---+---+---+\n"
+            + "m|   |   |   |   |   |   |   |   |   |   |   |   |   |   |\n"
+            + " +---+---+---+---+---+---+---+---+---+---+---+---+---+---+\n"
+            + "n|   |   |   |   |   |   |   |   |   |   |   |   |   |   |\n"
+            + " +---+---+---+---+---+---+---+---+---+---+---+---+---+---+\n";
+
+    /**
+     * Un plateau pour tester le niveau 2.
+     */
+    final String PLATEAU_NIVEAU2
+            = "   A   B   C   D   E   F   G   H   I   J   K   L   M   N \n"
+            + " +---+---+---+---+---+---+---+---+---+---+---+---+---+---+\n"
+            + "a|   |   |   |   |   |   |   |   |   |   |   |   |   |PB1|\n"
+            + " +---+---+---+---+---+---+---+---+---+---+---+---+---+---+\n"
+            + "b|   |   |   |   |   |   |   |   |   |   |   |   |   |   |\n"
+            + " +---+---+---+---+---+---+---+---+---+---+---+---+---+---+\n"
+            + "c|   |   |   |   |   |   |   |   |   |   |   |   |   |   |\n"
+            + " +---+---+---+---+---+---+---+---+---+---+---+---+---+---+\n"
+            + "d|   |   |   |   |   |   |   |   |   |   |   |   |   |   |\n"
+            + " +---+---+---+---+---+---+---+---+---+---+---+---+---+---+\n"
+            + "e|   |   |   |   |   |   |   |   |PR1|   |   |   |   |   |\n"
+            + " +---+---+---+---+---+---+---+---+---+---+---+---+---+---+\n"
+            + "f|PR1|   |   |   |   |   |   |   |   |   |   |   |   |   |\n"
+            + " +---+---+---+---+---+---+---+---+---+---+---+---+---+---+\n"
+            + "g|   |   |   |   |   |   |   |   |   |   |   |   |   |   |\n"
+            + " +---+---+---+---+---+---+---+---+---+---+---+---+---+---+\n"
+            + "h|   |   |   |   |   |   |   |   |   |PB1|   |   |   |   |\n"
+            + " +---+---+---+---+---+---+---+---+---+---+---+---+---+---+\n"
+            + "i|   |   |   |   |   |   |   |   |   |   |   |   |   |   |\n"
+            + " +---+---+---+---+---+---+---+---+---+---+---+---+---+---+\n"
+            + "j|   |   |   |   |   |   |   |   |   |   |   |   |   |   |\n"
+            + " +---+---+---+---+---+---+---+---+---+---+---+---+---+---+\n"
+            + "k|   |   |   |   |   |   |   |   |   |   |   |   |   |   |\n"
+            + " +---+---+---+---+---+---+---+---+---+---+---+---+---+---+\n"
+            + "l|   |   |   |   |   |   |   |   |   |   |   |   |   |   |\n"
+            + " +---+---+---+---+---+---+---+---+---+---+---+---+---+---+\n"
+            + "m|   |   |   |   |   |   |   |   |   |   |   |   |   |   |\n"
+            + " +---+---+---+---+---+---+---+---+---+---+---+---+---+---+\n"
+            + "n|   |   |   |   |   |   |   |   |   |   |   |   |   |   |\n"
+            + " +---+---+---+---+---+---+---+---+---+---+---+---+---+---+\n";
+}
