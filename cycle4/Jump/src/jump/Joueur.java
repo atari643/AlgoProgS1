31560f1dbdb7b6acda145ac66d28239189f77ec4
qartigala <qartigala@etu.iut-bordeaux.fr>
2022-11-29 10:31:17 +0100

    Ajout du cycle4

---
+++ cycle4/Jump/src/jump/Joueur.java
@@ -0,0 +1,117 @@
+package jump;
+
+/**
+ * Un joueur.
+ */
+class Joueur {
+
+    /**
+     * Nom du joueur.
+     */
+    String nom;
+
+    /**
+     * Colonne où se trouve le joueur.
+     */
+    int colonne;
+
+    /**
+     * Le client utilisé par ce joueur.
+     */
+    ICallback client;
+
+    /**
+     * Score.
+     */
+    int score;
+
+    /**
+     * Statut du joueur en cours de partie.
+     */
+    StatutJoueur statut;
+
+    /**
+     * Séparateur de champs pour la sérialisation/désérialisation.
+     */
+    final static String SEPARATEUR = "!";
+
+    /**
+     * Constructeur, sans client associé.
+     *
+     * @param unNom nom du joueur
+     */
+    Joueur(String unNom) {
+        this(unNom, null);
+    }
+
+    /**
+     * Constructeur.
+     *
+     * @param unNom nom du joueur
+     * @param unClient client du joueur
+     */
+    Joueur(String unNom, ICallback unClient) {
+        nom = unNom;
+        client = unClient;
+        score = 0;
+        statut = StatutJoueur.A_JOUE;
+    }
+
+    /**
+     * Retourne le nom tronqué sur 10 caractères (ou le nom s'il est composé de
+     * 10 caractères ou moins).
+     * 
+     * @param unNom le nom
+     * @return le nom tronqué
+     */
+    static String tronquerNom(String unNom) {
+        return ""; // TODO
+    }
+
+    /**
+     * Caractère représentant ce joueur sur le plateau.
+     *
+     * @return caractère représentant ce joueur sur le plateau
+     */
+    char caracterePlateau() {
+        return nom.charAt(0);
+    }
+
+    /**
+     * Indique si le nom du joueur est correct, c'est-à-dire s'il commence par
+     * une lettre majuscule non accentuée.
+     *
+     * @return vrai ssi le nom du joueur est correct
+     */
+    boolean nomCorrect() {
+        return true;
+    }
+
+    /**
+     * Sérialiser un joueur.
+     *
+     * @return une chaîne représentant ce joueur
+     */
+    String serialiser() {
+        return "";
+    }
+
+    /**
+     * Désérialiser un joueur.
+     *
+     * @param serial la sérialisation du joueur
+     * @return le joueur obtenu à partir d'une sérialisation donnée.
+     */
+    static Joueur deserialiser(String serial) {
+        return null;
+    }
+    
+    /**
+     * Ligne correspondant à ce joueur dans le fichier de scores.
+     * 
+     * @return la ligne correspondant à ce joueur dans le fichier de scores
+     */
+    String ligneFichierScore() {
+        return "";
+    }
+}
