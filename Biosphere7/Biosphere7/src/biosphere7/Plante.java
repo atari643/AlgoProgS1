964695c95fe9373e0f04ecc440ef82f635722c7b
qartigala <qartigala@etu.iut-bordeaux.fr>
2022-11-30 12:35:00 +0100

    ajout enumération plante

---
+++ Biosphere7/Biosphere7/src/biosphere7/Plante.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
+ */
+package biosphere7;
+
+/**
+ *
+ * @author qartigala
+ */
+public enum Plante {
+    POMMIER,
+    SUREAU,
+    FRANBOISIER,
+    POMMESDETERRE,
+    TOMATES,
+    HARICOTS;
+    
+}
